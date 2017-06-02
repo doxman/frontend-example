5be8e58c3b80628df03621b11813f07b14870cb0
Daniel Oxman <zeoxyman@gmail.com>
2017-06-01 23:47:51 -0400

    Successfully pulling some photo objects from fake data server

--- app/routes/add.js
+++ app/routes/add.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) { // The only parameter is album_id, an integer ID for an album
+    return Ember.$.ajax({
+      // Hit an API for photo data
+      url: 'https://jsonplaceholder.typicode.com/photos?albumId=' + params.album_id,
+      method: 'GET'
+    }).then((data) => {
+      this.get('store').pushPayload({photo: data});
+    });
+  }
 });
